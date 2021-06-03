f7f4811255f50ff48202f256cfdcdf04c48b263f
yeniferbarco <>
2021-06-03 02:49:44 +0000

    Initial commit

---
+++ 3.0 Bajo el radar/main.py
@@ -0,0 +1,16 @@
+""" Programa Apoyo multas#
+    incorpora al modulo multas.py
+    Oscar Franco-Bedoya
+    Mayo 3-2021 """
+
+#---------------- Zona librerias------------
+import multas.py as mult
+
+#======================================================================
+#          E S P A C I O    D E    T R A B A J O     A L U M N O
+# ====================================================================
+
+
+#======================================================================
+#   Algoritmo principal Punto de entrada a la aplicación (Conquistar)
+# =====================================================================
---
+++ 3.0 Bajo el radar/multas.py
@@ -0,0 +1,22 @@
+""" Modulo Multas
+    Funciones para el cálculo de multas
+    de tránsito 
+    Oscar Franco-Bedoya
+    Mayo 10-2021 """
+# Definición de Funciones
+#======================================================================
+#          E S P A C I O    D E    T R A B A J O     A L U M N O
+# ====================================================================
+def multar_velocidad(distancia_uno, distancia_dos,tiempo):
+  #TODO: Documentar función 
+  texto_multa=""
+  #TODO: Implementar función
+  return texto_multa
+
+def multar_alcoholemia(grado_alcohol):
+#TODO: Documentar función 
+  texto_multa=""
+  #TODO: Implementar función
+  return texto_multa
+
+
